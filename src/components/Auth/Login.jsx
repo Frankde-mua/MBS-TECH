e1c326d77287580704093dfcb7d7a71b557b6339
Frankde-mua <frankluther3@gmail.com>
2025-10-20 07:57:41 +0200

    set local machine for hosting

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -16,7 +16,7 @@ const Login = ({ onLogin }) => {
     setLoading(true);
 
     try {
-      const res = await axios.post("http://localhost:5000/api/login", form);
+      const res = await axios.post("https://twelve-hornets-smell.loca.lt/api/login", form);
 
       if (res.data.success) {
         localStorage.setItem("user", JSON.stringify(res.data.user));
